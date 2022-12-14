4ef8400d227bec8a803e7fa3668f16cb9cc41370
Ilion Beyst <ilion.beyst@gmail.com>
2022-12-14 21:38:26 +0100

    Day 11: reformat & optimize

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -2,7 +2,15 @@ use std::cmp::Reverse;
 
 use crate::utils::Reader;
 
-fn calc_monkey_business_level(input_path: &str, n_rounds: usize, worry_divisor: usize) {
+pub fn part1(input_path: &str) {
+    calc_monkey_business_level(input_path, 20, Some(3));
+}
+
+pub fn part2(input_path: &str) {
+    calc_monkey_business_level(input_path, 10000, None);
+}
+
+fn calc_monkey_business_level(input_path: &str, n_rounds: usize, worry_divisor: Option<usize>) {
     let mut monkeys = parse_input(input_path);
 
     let base: usize = monkeys.iter().map(|m| m.test.divisor).product();
@@ -12,33 +20,96 @@ fn calc_monkey_business_level(input_path: &str, n_rounds: usize, worry_divisor: 
         for monkey_num in 0..monkeys.len() {
             num_inspections[monkey_num] += monkeys[monkey_num].items.len();
             while let Some(mut worry) = monkeys[monkey_num].items.pop() {
-                worry = monkeys[monkey_num].operation.run(worry);
-                worry /= worry_divisor;
+                worry = monkeys[monkey_num].operation.apply(worry);
+                if let Some(divisor) = worry_divisor {
+                    worry /= divisor;
+                }
                 worry %= base;
                 let target_monkey = monkeys[monkey_num].test.get_target_monkey(worry);
                 monkeys[target_monkey].items.push(worry);
             }
         }
-
-        // println!(
-        //     "After round {}, the monkeys are holding items with these worry levels:",
-        //     round
-        // );
-        // for (num, monkey) in monkeys.iter().enumerate() {
-        //     println!("Monkey {}: {:?}", num, monkey.items);
-        // }
-        // println!();
     }
     num_inspections.sort_by_key(|&n| Reverse(n));
     println!("{}", num_inspections[0] * num_inspections[1]);
 }
 
-pub fn part1(input_path: &str) {
-    calc_monkey_business_level(input_path, 20, 3);
+struct Monkey {
+    items: Vec<usize>,
+    operation: MonkeyOp,
+    test: Test,
 }
 
-pub fn part2(input_path: &str) {
-    calc_monkey_business_level(input_path, 10000, 1);
+enum MonkeyOp {
+    AddConst(usize),
+    MulConst(usize),
+    Square,
+}
+
+impl MonkeyOp {
+    fn apply(&self, old: usize) -> usize {
+        match self {
+            MonkeyOp::AddConst(num) => old + num,
+            MonkeyOp::MulConst(num) => old * num,
+            MonkeyOp::Square => old * old,
+        }
+    }
+}
+
+struct Test {
+    divisor: usize,
+    true_target: usize,
+    false_target: usize,
+}
+
+impl Test {
+    fn get_target_monkey(&self, worry_level: usize) -> usize {
+        if worry_level % self.divisor == 0 {
+            self.true_target
+        } else {
+            self.false_target
+        }
+    }
+}
+
+// this struct is used only for parsing
+struct Operation {
+    left: Operand,
+    right: Operand,
+    operator: Operator,
+}
+
+enum Operand {
+    Old,
+    Const(usize),
+}
+
+enum Operator {
+    Add,
+    Mul,
+}
+
+// "compile" the monkey operation into a more efficient form,
+// that can be evaluated with less branching.
+impl Operation {
+    fn into_monkey_op(self) -> MonkeyOp {
+        use Operand::*;
+        use Operator::*;
+
+        let mut old = self.left;
+        let mut other = self.right;
+        if matches!(other, Old) {
+            std::mem::swap(&mut old, &mut other);
+        }
+        assert!(matches!(old, Old));
+
+        match (self.operator, other) {
+            (Add, Const(num)) => MonkeyOp::AddConst(num),
+            (Mul, Const(num)) => MonkeyOp::MulConst(num),
+            (Add, Old) => MonkeyOp::MulConst(2),
+            (Mul, Old) => MonkeyOp::Square,
+        }
+    }
 }
 
 fn parse_input(input_path: &str) -> Vec<Monkey> {
@@ -63,7 +134,7 @@ fn read_monkey(reader: &mut Reader) -> Monkey {
     reader.skip_lit(b"\n");
 
     let items = read_items(reader);
-    let operation = read_operation(reader);
+    let operation = read_operation(reader).into_monkey_op();
     let test = read_test(reader);
     Monkey {
         items,
@@ -93,7 +164,6 @@ fn read_operation(reader: &mut Reader) -> Operation {
     reader.skip_lit(b" ");
     let operator = match reader.peek() {
         b'+' => Operator::Add,
-        b'-' => Operator::Sub,
         b'*' => Operator::Mul,
         c => panic!("unknown operator {}", c),
     };
@@ -115,7 +185,7 @@ fn read_operand(reader: &mut Reader) -> Operand {
         Operand::Old
     } else {
         let num = reader.read_usize();
-        Operand::Number(num)
+        Operand::Const(num)
     }
 }
 
@@ -130,71 +200,5 @@ fn read_test(reader: &mut Reader) -> Test {
         divisor,
         true_target,
         false_target,
-    }
-}
-
-struct Monkey {
-    items: Vec<usize>,
-    operation: Operation,
-    test: Test,
-}
-
-struct Operation {
-    left: Operand,
-    right: Operand,
-    operator: Operator,
-}
-
-impl Operation {
-    fn run(&self, old_value: usize) -> usize {
-        let left = self.left.get_value(old_value);
-        let right = self.right.get_value(old_value);
-        self.operator.apply(left, right)
-    }
-}
-
-enum Operand {
-    Old,
-    Number(usize),
-}
-
-impl Operand {
-    fn get_value(&self, old_value: usize) -> usize {
-        match self {
-            Operand::Old => old_value,
-            Operand::Number(value) => *value,
-        }
-    }
-}
-
-enum Operator {
-    Add,
-    Sub,
-    Mul,
-}
-
-impl Operator {
-    fn apply(&self, left: usize, right: usize) -> usize {
-        match self {
-            Operator::Add => left + right,
-            Operator::Sub => left - right,
-            Operator::Mul => left * right,
-        }
-    }
-}
-
-struct Test {
-    divisor: usize,
-    true_target: usize,
-    false_target: usize,
-}
-
-impl Test {
-    fn get_target_monkey(&self, worry_level: usize) -> usize {
-        if worry_level % self.divisor == 0 {
-            self.true_target
-        } else {
-            self.false_target
-        }
     }
 }
